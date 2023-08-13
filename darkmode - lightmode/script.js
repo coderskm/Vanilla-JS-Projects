029a9d0d678b211c396c00db8047c0ab871a1042
Sumit Kumar Mishra <sumitkumarmishra.code@gmail.com>
2023-08-13 11:12:55 +0530

    done

--- darkmode - lightmode/script.js
+++ darkmode - lightmode/script.js
@@ -1,19 +1,14 @@
-let btn = document.getElementById('btn');
+let btn = document.getElementById("btn");
 
 function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
 }
 
-btn.addEventListener('click', () => {
-    if (localStorage.getItem(`theme`) === 'dark-theme') {
-        setTheme('light-theme');
-    } else {
-        setTheme("dark-theme");
-    }
-
-})
-
-    (function () {
-    
-})()
+btn.addEventListener("click", () => {
+  if (localStorage.getItem(`theme`) === "dark-theme") {
+    setTheme("light-theme");
+  } else {
+    setTheme("dark-theme");
+  }
+});
